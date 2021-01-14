e4b8da28a4cd24d68c4da101718cbe2555617d6b
Jason Kim <dialogbox@gmail.com>
2021-01-14 12:47:35 +0900

    Upgrade reqwest to 0.11

--- examples/top.rs
+++ examples/top.rs
@@ -1,38 +1,74 @@
-use hn_api::HnClient;
+use futures::future::{join_all, OptionFuture};
+use hn_api::{
+    types::{Item, User},
+    HnClient,
+};
 
-fn print(api: &HnClient, items: &[u32]) {
-    for id in items {
-        let item = api.get_item(*id).unwrap().unwrap();
-        let author = item.author().map(|username| {
-            let user = api.get_user(username).unwrap().unwrap();
-            format!("{}, karma {}", username, user.karma)
-        });
-        println!(
-            "- {}: {} (by {})",
-            item.id(),
-            item.title().unwrap_or("?"),
-            author.unwrap_or_else(|| "?".to_string()),
-        )
+async fn get_item_and_author(
+    api: &HnClient,
+    id: u32,
+) -> reqwest::Result<(Option<Item>, Option<User>)> {
+    let item = api.get_item(id).await?;
+
+    let author_future: OptionFuture<_> = item
+        .as_ref()
+        .and_then(|item| item.author().map(|a| a.to_string()))
+        .map(|a| api.get_user(a))
+        .into();
+
+    let author = author_future.await.transpose()?.flatten();
+
+    Ok((item, author))
+}
+
+async fn get_items<'a, I>(
+    api: &HnClient,
+    items: I,
+) -> reqwest::Result<Vec<(Option<Item>, Option<User>)>>
+where
+    I: IntoIterator<Item = &'a u32>,
+{
+    join_all(items.into_iter().map(|id| get_item_and_author(api, *id)))
+        .await
+        .into_iter()
+        .collect()
+}
+
+async fn print(api: &HnClient, items: &[u32]) {
+    let items = get_items(api, items).await.expect("Can not retrive items");
+
+    for (item, user) in items {
+        let author = user.map(|user| format!("{}, karma {}", user.id, user.karma));
+
+        if let Some(item) = item {
+            println!(
+                "- {}: {} (by {})",
+                item.id(),
+                item.title().unwrap_or("?"),
+                author.unwrap_or_else(|| "?".to_string()),
+            );
+        };
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("What's new on HN:\n");
 
     let api = HnClient::init().unwrap();
 
-    let top = api.get_top_stories().unwrap();
-    let new = api.get_new_stories().unwrap();
-    let best = api.get_best_stories().unwrap();
+    let top = api.get_top_stories().await.unwrap();
+    let new = api.get_new_stories().await.unwrap();
+    let best = api.get_best_stories().await.unwrap();
 
     let count = 3;
 
     println!("Top {} stories:", count);
-    print(&api, &top[..count]);
+    print(&api, &top[..count]).await;
 
     println!("\nNewest count stories:");
-    print(&api, &new[..count]);
+    print(&api, &new[..count]).await;
 
     println!("\nBest count stories:");
-    print(&api, &best[..count]);
+    print(&api, &best[..count]).await;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,7 @@ pub struct HnClient {
 }
 
 impl HnClient {
-
+    
     /// Create a new `HnClient` instance.
     pub fn init() -> reqwest::Result<Self> {
         let client = reqwest::Client::builder()
@@ -58,57 +58,57 @@ impl HnClient {
     /// Return the item with the specified id.
     ///
     /// May return `None` if item id is invalid.
-    pub fn get_item(&self, id: u32) -> reqwest::Result<Option<types::Item>> {
-        self.client.get(&format!("{}/item/{}.json", API_BASE_URL, id)).send()?.json()
+    pub async fn get_item(&self, id: u32) -> reqwest::Result<Option<types::Item>> {
+        self.client.get(&format!("{}/item/{}.json", API_BASE_URL, id)).send().await?.json().await
     }
 
     /// Return the user with the specified username.
     ///
     /// May return `None` if username is invalid.
-    pub fn get_user(&self, username: &str) -> reqwest::Result<Option<types::User>> {
-        self.client.get(&format!("{}/user/{}.json", API_BASE_URL, username)).send()?.json()
+    pub async fn get_user(&self, username: String) -> reqwest::Result<Option<types::User>> {
+        self.client.get(&format!("{}/user/{}.json", API_BASE_URL, username)).send().await?.json().await
     }
 
     /// Return the id of the newest item.
     ///
     /// To get the 10 latest items, you can decrement the id 10 times.
-    pub fn get_max_item_id(&self) -> reqwest::Result<u32> {
-        self.client.get(&format!("{}/maxitem.json", API_BASE_URL)).send()?.json()
+    pub async fn get_max_item_id(&self) -> reqwest::Result<u32> {
+        self.client.get(&format!("{}/maxitem.json", API_BASE_URL)).send().await?.json().await
     }
 
     /// Return a list of top story item ids.
-    pub fn get_top_stories(&self) -> reqwest::Result<Vec<u32>> {
-        self.client.get(&format!("{}/topstories.json", API_BASE_URL)).send()?.json()
+    pub async fn get_top_stories(&self) -> reqwest::Result<Vec<u32>> {
+        self.client.get(&format!("{}/topstories.json", API_BASE_URL)).send() .await?.json().await
     }
 
     /// Return a list of new story item ids.
-    pub fn get_new_stories(&self) -> reqwest::Result<Vec<u32>> {
-        self.client.get(&format!("{}/newstories.json", API_BASE_URL)).send()?.json()
+    pub async fn get_new_stories(&self) -> reqwest::Result<Vec<u32>> {
+        self.client.get(&format!("{}/newstories.json", API_BASE_URL)).send().await?.json().await
     }
 
     /// Return a list of best story item ids.
-    pub fn get_best_stories(&self) -> reqwest::Result<Vec<u32>> {
-        self.client.get(&format!("{}/beststories.json", API_BASE_URL)).send()?.json()
+    pub async fn get_best_stories(&self) -> reqwest::Result<Vec<u32>> {
+        self.client.get(&format!("{}/beststories.json", API_BASE_URL)).send().await?.json().await
     }
 
     /// Return up to 200 latest Ask HN story item ids.
-    pub fn get_ask_stories(&self) -> reqwest::Result<Vec<u32>> {
-        self.client.get(&format!("{}/askstories.json", API_BASE_URL)).send()?.json()
+    pub async fn get_ask_stories(&self) -> reqwest::Result<Vec<u32>> {
+        self.client.get(&format!("{}/askstories.json", API_BASE_URL)).send().await?.json().await
     }
 
     /// Return up to 200 latest Show HN story item ids.
-    pub fn get_show_stories(&self) -> reqwest::Result<Vec<u32>> {
-        self.client.get(&format!("{}/showstories.json", API_BASE_URL)).send()?.json()
+    pub async fn get_show_stories(&self) -> reqwest::Result<Vec<u32>> {
+        self.client.get(&format!("{}/showstories.json", API_BASE_URL)).send().await?.json().await
     }
 
     /// Return up to 200 latest Job story item ids.
-    pub fn get_job_stories(&self) -> reqwest::Result<Vec<u32>> {
-        self.client.get(&format!("{}/jobstories.json", API_BASE_URL)).send()?.json()
+    pub async fn get_job_stories(&self) -> reqwest::Result<Vec<u32>> {
+        self.client.get(&format!("{}/jobstories.json", API_BASE_URL)).send().await?.json().await
     }
 
     /// Return a list of items and users that have been updated recently.
-    pub fn get_updates(&self) -> reqwest::Result<types::Updates> {
-        self.client.get(&format!("{}/updates.json", API_BASE_URL)).send()?.json()
+    pub async fn get_updates(&self) -> reqwest::Result<types::Updates> {
+        self.client.get(&format!("{}/updates.json", API_BASE_URL)).send().await?.json().await
     }
 
 }
