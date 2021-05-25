4c66d183f82d88f3e6c5fcac2df5c2823f997548
Dan Bruder <danb@structionsite.com>
2021-05-25 18:46:20 -0400

    comments

--- src/result.rs
+++ src/result.rs
@@ -1,11 +1,11 @@
-//! Results
+//! Errors, type aliases, and functions related to working with `Result`.
 
 use thiserror::Error;
 
 /// Result
 pub type Result<T> = std::result::Result<T, Error>;
 
-/// Error
+/// Represents all the ways that a request can fail.
 #[derive(Error, Debug)]
 pub enum Error {
     /// ReqwestError
