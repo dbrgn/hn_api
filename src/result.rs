12037258cae3b48400faedfce5850d38be2681f9
Dan Bruder <danb@structionsite.com>
2021-05-26 08:14:13 -0400

    remove thiserror, its not adding much in this case

--- src/result.rs
+++ src/result.rs
@@ -1,14 +1,17 @@
 //! Errors, type aliases, and functions related to working with `Result`.
 
-use thiserror::Error;
-
 /// Result
 pub type Result<T> = std::result::Result<T, Error>;
 
 /// Represents all the ways that a request can fail.
-#[derive(Error, Debug)]
+#[derive(Debug)]
 pub enum Error {
     /// ReqwestError
-    #[error(transparent)]
-    ReqwestError(#[from] reqwest::Error),
+    ReqwestError(reqwest::Error),
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Self::ReqwestError(err)
+    }
 }
